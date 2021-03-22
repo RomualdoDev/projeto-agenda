5f829a6aad1b00676ff60c0631bf8622042a4579
Gabriel <gabrielsantosnep@hotmail.com>
2021-03-22 13:11:25 -0300

    cadastro de contatos

--- routes.js
+++ routes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const route = express.Router();
 const loginController = require('./src/controllers/loginController');
 const homeController = require('./src/controllers/homeController');
+const contatoController = require('./src/controllers/contatoController');
+
+const {loginRequired} = require('./src/middlewares/middleware');
 
 
 // Rotas da home
@@ -10,4 +13,12 @@ route.get('/', homeController.index);
 // Rotas de login
 route.get('/login/index', loginController.index);
 route.post('/login/register', loginController.register);
+route.post('/login/login', loginController.login);
+route.get('/login/logout', loginController.logout);
+
+//Rotas de Contato
+route.get('/contato/index', loginRequired, contatoController.index);
+route.post('/contato/register', loginRequired, contatoController.register);
+route.get('/contato/index/:id', loginRequired, contatoController.editIndex);
+
 module.exports = route;
